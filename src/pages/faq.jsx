70cef58bd4ed357a37ee21c3fd153a24e117682a
andi <wanndisng@gmail.com>
2026-03-13 15:59:59 +0700

    menambahkan data user

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Faq() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white-to-br from-slate-900 via-indigo-900 to-slate-800 text-white px-4">
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-slate-900 via-indigo-900 to-slate-800 text-white px-4">
       <div className="max-w-xl text-center">
         <h1 className="text-3xl font-bold mb-4">Kata kata hari ini</h1>
         <p className="text-lg">Ilove you hasian. Jgn lesu trs ya pudan;-</p>
