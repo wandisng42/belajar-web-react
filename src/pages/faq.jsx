bd5eaa4558010cec2d1f95f9220a549202b39e94
andi <wanndisng@gmail.com>
2026-03-13 15:31:10 +0700

    menambahkan data user

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -4,7 +4,7 @@ export default function Faq() {
   return (
     <div className="bg-linear-to-br from-slate-900 via-indigo-900 to-slate-800 text-white">
       <h1 className="text-3xl font-bold mb-4">Kata kata hari ini</h1>
-      <p className="text-lg">Ilove you hasian. Jgn lupa makan ya pudan;-</p>
+      <p className="text-lg">Ilove you hasian. Jgn lesu trs ya pudan;-</p>
     </div>
   );
 }
